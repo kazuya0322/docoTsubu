43588de74a9621f1e219a9e42c36a9987403c60d
takayama0322 <kazuya.takayama18@gmail.com>
2021-08-22 17:53:54 +0900

    ゼロ版　新規作成

---
+++ src/main/java/model/User.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private String name;
+	private String pass;
+
+	/**
+	 * Default constructor.
+	 */
+	public User() {
+		// Auto-generated constructor stub
+	}
+
+	public User(String name, String pass) {
+		this.name = name;
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+}
---
+++ src/main/java/servlet/Main.java
@@ -0,0 +1,94 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Mutter;
+import model.PostMutterLogic;
+import model.User;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/Main")
+public class Main extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Main() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// つぶやきリストをアプリケーションスコープから取得
+		ServletContext application = this.getServletContext();
+		List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+
+		// 取得できなかったら、つぶやきリストを新規作成してAPスコープに保存
+		if (mutterList == null) {
+			mutterList = new ArrayList<>();
+			application.setAttribute("mutterList", mutterList);
+		}
+		
+		// ログインしているか確認するためセッションスコープからユーザ情報取得
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+		
+		if (loginUser == null) {
+			response.sendRedirect("/docoTsubu/");
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+		
+		if(text != null && text.length() != 0) {
+			ServletContext application = request.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
+			
+			HttpSession session = request.getSession();
+			User loginUser = (User)session.getAttribute("loginUser");
+			
+			Mutter mutter = new Mutter(loginUser.getName(),text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+			
+			application.setAttribute("mutterList", mutterList);
+			
+		}
+		
+		// メイン画面へフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
